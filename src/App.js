4efdc40f19f44d6526700771b65f811fc29e16f4
Kaniah Dunn <kaniahdunn@gmail.com>
2021-06-12 14:38:41 -0400

    adding initial files

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './App.css';
+import Row from './Row';
+import requests from './requests';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Netflix Clone </h1>
+      <Row title="Netflix Originals" fetchUrl={requests.fetchNetflixOriginals} /> 
+      <Row title="Trending Now" fetchUrl={requests.fetchTrending}/>
+
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Row.js
@@ -0,0 +1,33 @@
+import React, { useState, useEffect } from 'react';
+import axios from './axios';
+
+function Row({title, fetchUrl}) {
+    const [movies, setMovies] = useState([]);
+
+    useEffect(() => {
+        async function fetchData( ){
+            const request = await axios.get(fetchUrl);
+            setMovies(request.data.results)
+            return request;
+        }
+        fetchData();
+    }, [fetchUrl]);
+
+    console.table(movies)
+    return (
+        <div className="row">
+            {/* title */}
+            <h2> {title} </h2>
+            {/* container */}
+            <div className="row_posters">
+                {movies.map(movie => (
+                <img src={movie.poster_path} alt={movie.name}>
+                </img>
+                ))}
+
+            </div>
+        </div>
+    )
+}
+
+export default Row
