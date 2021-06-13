81da39b19e1d8b5a88bb0d4134580dc68814f3b0
Kaniah Dunn <kaniahdunn@gmail.com>
2021-06-13 15:57:31 -0400

    building and styling banner

--- src/Banner.js
+++ src/Banner.js
@@ -1,6 +1,8 @@
 import React, { useEffect, useState } from "react";
 import axios from "./axios";
 import requests from "./requests";
+import "./Banner.css";
+
 function Banner() {
   const [movie, setMovie] = useState([]);
 
@@ -16,15 +18,38 @@ function Banner() {
     }
     fetchData();
   }, []);
+
+  function truncate(str, n) {
+    return str?.length > n ? str.substr(0, n - 1) + "..." : str;
+  }
   return (
-    <header>
+    <header
+      className='banner'
+      style={{
+        backgroundSize: "cover",
+        backgroundImage: `url("https://image.tmdb.org/t/p/original/${movie?.backdrop_path}")`,
+        backgroundPosition: "center center",
+      }}
+    >
       {/* Background Image */}
+      <div className='banner_contents'>
+        {/* Title */}
+        <h1 className='banner_title'>
+          {movie?.title || movie?.name || movie?.original_name}
+        </h1>
+        {/* Div with 2 Buttons */}
+        <div className='banner_buttons'></div>
+        <button className='banner_button'>Play</button>
+        <button className='banner_button'>My List</button>
 
-      {/* Title */}
+        {/* Description */}
+        <h1 className='banner_description'>
+          {" "}
+          {truncate(movie?.overview, 150)}{" "}
+        </h1>
+      </div>
 
-      {/* Div with 2 Buttons */}
-
-      {/* Description */}
+      <div className='banner--fadeBottom'></div>
     </header>
   );
 }
--- src/requests.js
+++ src/requests.js
@@ -1,14 +1,14 @@
-const API_KEY = '09f67b8cef0c554ad34d351df0aab2ee';
+const API_KEY = "09f67b8cef0c554ad34d351df0aab2ee";
 
 const requests = {
-    fetchTrending: `/trending/all/week?api_key=${API_KEY}&language=end-US`,
-    fetchNetflixOriginals: `/discover/tv?api_key=${API_KEY}&with_networks=123`,
-    fetchTopRated: `/movie/top_rated?api_key=${API_KEY}&language=end-US`, 
-    fetchActionMovies: `/discover/movie?api_key=${API_KEY}&with_genres=28`,
-    fetchComedyMovies: `/discover/movie?api_key=${API_KEY}&with_genres=35`,
-    fetchHorrorMovies: `/discover/movie?api_key=${API_KEY}&with_genres=27`,
-    fetchRomanceMovies: `/discover/movie?api_key=${API_KEY}&with_genres=10749`,
-    fetchDocumentaries: `/discover/movie?api_key=${API_KEY}&with_genres=99`,
-}
+  fetchTrending: `/trending/all/week?api_key=${API_KEY}&language=end-US`,
+  fetchNetflixOriginals: `/discover/tv?api_key=${API_KEY}&with_networks=123`,
+  fetchTopRated: `/movie/top_rated?api_key=${API_KEY}&language=end-US`,
+  fetchActionMovies: `/discover/movie?api_key=${API_KEY}&with_genres=28`,
+  fetchComedyMovies: `/discover/movie?api_key=${API_KEY}&with_genres=35`,
+  fetchHorrorMovies: `/discover/movie?api_key=${API_KEY}&with_genres=27`,
+  fetchRomanceMovies: `/discover/movie?api_key=${API_KEY}&with_genres=10749`,
+  fetchDocumentaries: `/discover/movie?api_key=${API_KEY}&with_genres=99`,
+};
 
 export default requests;
